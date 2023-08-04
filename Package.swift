644e96f928766dd3df94a45f1ea77d80cb610473
Jasper Valdivia <28489757+jasval@users.noreply.github.com>
2023-08-04 17:13:46 +0100

    Modify Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@
 import PackageDescription
 let package = Package(
 	name: "SwiftBurntChroma",
+	platforms: [
+		.iOS(.v15),
+		.macOS(.v12),
+		.watchOS(.v8),
+		.tvOS(.v15)
+	],
 	products: [
 		.library(
 			name: "SwiftBurntChroma",
